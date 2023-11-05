6b8c47ffea3e4bfc1cd83708537d64eacd3b3692
JuliVarshney <112109262+JuliVarshney@users.noreply.github.com>
2023-11-05 16:18:23 +0530

    backend project

---
+++ CRS-Backend1/src/main/java/com/crs/CrsBackend1Application.java
@@ -0,0 +1,13 @@
+package com.crs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrsBackend1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrsBackend1Application.class, args);
+	}
+
+}
